d49027d4407c965f44083d654fa96f561d3dadd4
Rafael <rapha.pse@outlook.com>
2019-10-25 20:49:51 -0300

    Fix color

--- crm-solutions/src/app/login/login.component.ts
+++ crm-solutions/src/app/login/login.component.ts
@@ -7,30 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  // public card = {
-  //   iconTye: 'mala',
-  //   cardValue: '5%',
-  //   countActions: 64,
-  //   information: 'Leads'
-  // };
+  public card = {
+    iconTye: 'mala',
+    cardValue: '5%',
+    countActions: 64,
+    information: 'Leads'
+  };
 
-  // // recebe da API
-  // public list = [
-  //   {name: 'Dashboard', icon: 'dashboard'},
-  //   {name: 'Deals', icon: 'peaple-cicle'},
-  //   {name: 'Contacts', icon: 'star'},
-  //   {name: 'Stats', icon: 'timeline'},
-  //   {name: 'Settings', icon: 'setting'},
-  // ];
+  // recebe da API
+  public list = [
+    {name: 'Dashboard', icon: 'dashboard'},
+    {name: 'Deals', icon: 'peaple-cicle'},
+    {name: 'Contacts', icon: 'star'},
+    {name: 'Stats', icon: 'timeline'},
+    {name: 'Settings', icon: 'setting'},
+  ];
 
-  // public listSecundary = [
-  //   {name: 'Bryce Canyon a stunnin…', icon: 'mala'},
-  //   {name: 'What makes a hotel bou…', icon: 'mala'},
-  // ];
-  // public listaDefault = [
-  //   {name: 'Bryce Canyon a stunnin…', icon: 'client'},
-  //   {name: 'What makes a hotel bou…', icon: 'client'},
-  // ];
+  public listSecundary = [
+    {name: 'Bryce Canyon a stunnin…', icon: 'mala'},
+    {name: 'What makes a hotel bou…', icon: 'mala'},
+  ];
+  public listaDefault = [
+    {name: 'Bryce Canyon a stunnin…', icon: 'client'},
+    {name: 'What makes a hotel bou…', icon: 'client'},
+  ];
 
   public lisN = [11.2, 22.5, 44.1, 100];
   public lisLs = ['Mail', 'Phone', 'Site', 'API'];
